2136ac84decf21322dccb1572f7298ba3961f0db
saksham17-tech <msaksham771@gmail.com>
2025-09-06 22:51:16 +0530

    Add files via upload
    INCREASING NUMBER PYRAMID.

---
+++ star2.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class star2
+{
+    public static void main(String[] sm)
+
+    {
+        //hollow square
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of your pattern: ");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
