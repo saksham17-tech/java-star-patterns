189e1af6de930c530cf23d34b45609cb9ff053dd
saksham17-tech <msaksham771@gmail.com>
2025-09-07 13:17:29 +0530

    star5
    NUMBER CHANGING PYRAMID

---
+++ star5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class star5 
+{
+        public static void main(String[] sm) 
+        {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of your pattern: ");
+        int n=sc.nextInt();
+        System.out.println();
+        int k=1;
+        for(int i=1;i<=n;i++)
+        {                 
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(k+ " ");
+                k++;
+            }
+            System.out.println();
+        }
+}
+}
