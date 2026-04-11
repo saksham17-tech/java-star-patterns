334b72ae8e3c722d4d6bf06cdf927df69010ef4a
saksham17-tech <msaksham771@gmail.com>
2026-04-11 20:35:39 +0530

    INCREASING LEFT TRIANGLE WITH NUMBERS

--- star2.java
+++ star2.java
@@ -14,7 +14,7 @@ public class star2
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
