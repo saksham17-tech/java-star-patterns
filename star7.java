63b2344747b4b8a5fd266d9e8ea65c34e7e8865d
saksham17-tech <msaksham771@gmail.com>
2026-04-11 21:01:27 +0530

    DECREASING NUMBER TRIANGLE
    Refactored the pattern printing logic to improve output format and added closing of the Scanner object.

--- star7.java
+++ star7.java
@@ -6,22 +6,18 @@ public class star7
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of your pattern: ");
         int s=sc.nextInt();
-        int y=0;
-        int p=s;
-        if(s%2!=0)
-        p++;
-        y=p/2;        
-        System.out.println();
         for(int i=0;i<s;i++)
         {                 
-            for(int j=0;j<s;j++)
+            for(int j=0;j<s-i-1;j++)
             {
-                if((i+j)<=(y))
                 System.out.print("  ");
-                if((i+j)>y)
-                System.out.print((j+1)+" ");
+            }
+            for(int k=s-i;k<=s;k++)
+            {
+                System.out.print(" "+k);
             }
             System.out.println();
         }
+        sc.close();
     }
 }
